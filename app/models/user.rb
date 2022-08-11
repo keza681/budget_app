2b3aadc5dab540e35f3cba9b309e29138e535006
keza681 <lindakeza19@gmail.com>
2022-08-12 00:52:59 +0200

    Add views to project

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
-    devise :database_authenticatable, :registerable,
-           :recoverable, :rememberable, :validatable
-  
-    validates :name, presence: true
-    validates :email, presence: true
-  
-    has_many :transactions
-    has_many :categories
-  end
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true
+  validates :email, presence: true
+
+  has_many :transactions
+  has_many :categories
+end
