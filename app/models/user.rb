f42aafe7b27b9d253dfde3388b684ee0557c6cbd
keza681 <lindakeza19@gmail.com>
2022-08-13 16:18:42 +0200

    Fix linters errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
-  :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable
 
   validates :name, presence: true
   validates :email, presence: true
